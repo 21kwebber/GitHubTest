266c03f4c0e1620c3fc09131e0e6f62dc56b72a2
Keith Webber <kcwebber02@gmail.com>
2024-01-29 17:08:00 -0500

    commit

--- MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b790b91baea2448f4eb77c25914fcaf6e570b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+638dc43f93bb9190d56991028a63dc02c9506915")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
